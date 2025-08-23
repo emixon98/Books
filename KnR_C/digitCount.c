7567aebb9652517c324abae494c07a98bf04fd6c
Elijah <emixon98@gmail.com>
2025-08-22 18:21:27 -0700

    3.4

---
+++ KnR_C/digitCount.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/*Count the digits in a file until we hit the end, we do this
+by merely checking if the current char matches 0-9
+count whitespace and every other character as well */
+
+int main()
+{
+    int c, i, nwhite, nother, ndigit[10];
+
+    nwhite = nother = 0;
+    for (i = 0; i < 10; i++)
+        ndigit[i] = 0;
+    while ((c = getchar()) != EOF) {
+        switch (c) {
+            case '0': case '1': case '2': case'3': case '4':
+            case '5': case '6': case '7': case '8': case '9':
+            ndigit[c-'0']++;
+            break;
+        case ' ':
+        case '\n':
+        case '\t':
+            nwhite++;
+            break;
+        default:
+            nother++;
+            break;
+        }
+    }
+    printf("digits =");
+    for (i=0; i<10; i++)
+        printf(" %d", ndigit[i]);
+    printf(", white space = %d, other = %d\n", nwhite, nother);
+    return 0;
+}
